993bfbffb10973f8649ae936a32173e72ce988ef
theone201323 <79582701+theone201323@users.noreply.github.com>
2021-03-07 14:46:54 +0800

    Add files via upload

---
+++ 2021.02.26 回家作業2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 14:45:52 2021
+
+@author: Luke Lin
+"""
+
+total = 0
+number = int(input("請輸入次數:"))
+for i in range(1,number+1):
+    # if i % 2 == 0:
+    total += i
+print(total)
