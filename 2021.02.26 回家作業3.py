a4d15f7cbff2c837ce558820d48ef467ea64492a
theone201323 <79582701+theone201323@users.noreply.github.com>
2021-03-07 14:59:47 +0800

    Add files via upload

---
+++ 2021.02.26 回家作業3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 19:08:12 2021
+
+@author: USER
+"""
+
+for i in range(1,5):
+    for a in range(1,i+1):
+        print(a,end='')
+    print()
+print("程式執行完畢")
